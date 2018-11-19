ea282a851fb045c61ed1520bd99ce118aa9ee2ad
pabristow <pbristow@hetp.u-net.com>
2018-11-19 16:28:21 +0000

    Enhanced versions() function

--- example/quan_simple.cpp
+++ example/quan_simple.cpp
@@ -3,27 +3,43 @@
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// Copyright Paul A. Bristow 1998, 2012.
+// Copyright Paul A. Bristow 1998, 2012, 2018.
 
-
-#include <boost/version.hpp>
+#include <boost/version.hpp> // macros BOOST_COMPILER, BOOST_VERSION, BOOST_STDLIB etc
 #include <boost/quan/unc.hpp>
 #include <boost/quan/unc_init.hpp>
 
 #include <iostream>
 
+//! Create a std::string containing boost and platform versions.
 std::string versions()
 {
   std::stringstream mess;
   mess << "Program: " __FILE__  << "\n";
-#ifdef __TIMESTAMP__
+  #ifdef __TIMESTAMP__
   mess << __TIMESTAMP__;
-#endif
+  #endif
   mess << "\nBuildInfo:\n" "  Platform " << BOOST_PLATFORM;
   mess << "\n  Compiler " BOOST_COMPILER;
-#ifdef _MSC_FULL_VER
-  mess << "\n  MSVC version "<< BOOST_STRINGIZE(_MSC_FULL_VER) << ".";
-#endif
+  #ifdef _MSC_FULL_VER
+    mess << "\n  MSVC version "<< BOOST_STRINGIZE(_MSC_FULL_VER) << ".";
+  #endif
+
+  #ifdef __GNUC__
+  // mess << __GNUC__ << '.' << __GNUC_MINOR__ <<'.' 
+    mess << "GCC 
+    #ifdef __cplusplus
+      mess << " C++ ";
+    #else
+      mess << " C ";
+    #endif
+    version " << __VERSION__ ;
+  #endif
+
+  #ifdef __clang__
+    mess << "clang " << __clang_major__ << '.' << __clang_minor__ << '.' __GNUC_PATCHLEVEL__;
+  #endif
+
   mess << "\n  STL " BOOST_STDLIB;
   mess << "\n  Boost version " << BOOST_VERSION/100000 << "." << BOOST_VERSION/100 % 1000 << "." << BOOST_VERSION % 100 << std::endl;
   return mess.str();
